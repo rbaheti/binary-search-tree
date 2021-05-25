7884c63cb797e32bce23f5ab7eb7ca0dd370b607
Rashmi Baheti <baheti.rash123@gmail.com>
2021-05-24 20:49:27 -0700

    Build the binary search tree.
    Visually it still does not look like a binary tree. Need to figure out how to properly positions the nodes to make it look like a binary tree.

--- src/BinaryTree.js
+++ src/BinaryTree.js
@@ -13,18 +13,106 @@ import { v4 as uuidv4 } from "uuid";
 export default class BinaryTree extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      myGraph: {
+        nodes: {},
+        edges: [],
+      },
+    };
   }
 
+  componentDidMount = () => {
+    this.generateDefaultGraph();
+  };
+
+  addVertex = (myGraph, num, id, parentId) => {
+    const label = num + "";
+
+    let node = { id, label, num, left: null, right: null };
+    if (Object.values(myGraph.nodes).length === 0) {
+      node.parentNum = null;
+      node.parentId = null;
+      node.level = 0;
+      node.y = node.level;
+
+      console.log("setting rootId: ", id);
+      myGraph.rootId = id;
+    } else {
+      let parentNode = myGraph.nodes[parentId];
+      node.parentNum = parentNode.num;
+      node.parentId = parentId;
+      node.level = parentNode.level + 1;
+      node.y = node.level;
+
+      if (num <= parentNode.num) {
+        parentNode.left = id;
+      } else {
+        parentNode.right = id;
+      }
+    }
+    myGraph.nodes[id] = node;
+  };
+
+  addEdge = (myGraph, id, source, target, label) => {
+    myGraph.edges.push({ id, source, target, label });
+  };
+
+  addValue = (num) => {
+    this.setState((previousState) => {
+      let myGraph = previousState.myGraph;
+      const id = uuidv4();
+      if (Object.values(myGraph.nodes).length === 0) {
+        this.addVertex(myGraph, num, id, null);
+        return { myGraph };
+      }
+
+      let parentId = myGraph.rootId;
+      while (true) {
+        let parentNode = myGraph.nodes[parentId];
+        console.log("num: ", num, " parent num: ", parentNode.num);
+        if (num <= parentNode.num) {
+          if (parentNode.left === null) {
+            this.addVertex(myGraph, num, id, parentId);
+            this.addEdge(myGraph, "edge-" + parentId + "-left", parentId, id, "left");
+            break;
+          } else {
+            parentId = parentNode.left;
+          }
+        } else {
+          if (parentNode.right === null) {
+            this.addVertex(myGraph, num, id, parentId);
+            this.addEdge(myGraph, "edge-" + parentId + "-right", parentId, id, "right");
+            break;
+          } else {
+            parentId = parentNode.right;
+          }
+        }
+      }
+
+      return { myGraph };
+    });
+  };
+
+  generateDefaultGraph = () => {
+    this.addValue(4);
+    this.addValue(2);
+    this.addValue(1);
+    this.addValue(3);
+    this.addValue(6);
+    this.addValue(5);
+    this.addValue(7);
+    this.addValue(8);
+    this.addValue(9);
+  };
+
   render = () => {
-    let myGraph = {
-      nodes: [
-        { id: "n1", label: "Alice" },
-        { id: "n2", label: "Rabbit" },
-      ],
-      edges: [{ id: "e1", source: "n1", target: "n2", label: "SEES" }],
-    };
+    console.log("this.state: ", this.state);
+    let myGraph = {};
+    myGraph["nodes"] = Object.values(this.state.myGraph.nodes);
+    myGraph["edges"] = this.state.myGraph.edges;
+    console.log("myGraph: ", myGraph);
     return (
-      <div>
+      <div style={{ width: "90%", margin: "auto", padding: "20px" }}>
         <Sigma
           graph={myGraph}
           key={myGraph.nodes.length + "hey"}
@@ -44,6 +132,7 @@ export default class BinaryTree extends Component {
           onClickNode={this.onClickNodeFunc}
         >
           <RelativeSize initialSize={15} />
+          <RandomizeNodePositions />
         </Sigma>
       </div>
     );
